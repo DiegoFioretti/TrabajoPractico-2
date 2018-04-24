d98459a3540fcdbd5fb8448860f9479ebad9483f
DiegoFioretti <diego_fioretti_97@hotmail.com>
2018-04-24 17:44:51 -0300

    Arreglo-1
    -Ventana de tamaño customizable
    -Sprite con textura y velocidad de movimiento customizable

--- SFML_Game/Main.cpp
+++ SFML_Game/Main.cpp
@@ -1,18 +1,26 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/System/Clock.hpp>
 #include <iostream>
 using namespace std;
 int windowHeight=500;
 int windowWidth=1000;
+int playerSpeed = 50;
+int gravity = 9.10;
 
 sf::Texture texture;
-sf::Sprite sprite;
+sf::Sprite player;
+//sf::Clock clock;
 
 void CreateTexture() {
-	if (!texture.loadFromFile("dog.png")){
-		// error...
+	if (!texture.loadFromFile("Resource/dog.png"))
+	{
+		std::cout << "Load failed" << std::endl;
+
+		system("pause");
 	}
 }
-void SetSprite() {
+void SetSprite(sf::Sprite &sprite) {
 	sprite.setTexture(texture);
 	/*
 	// position
@@ -28,24 +36,46 @@ void SetSprite() {
 	sprite.scale(sf::Vector2f(1.5f, 3.f)); // factor relative to the current scale
 	*/
 }
+void PlayerMovement() {
+	//player.move(0, gravity);
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		player.move(-playerSpeed, 0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		player.move(playerSpeed, 0);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		player.move(0, -playerSpeed);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		player.move(0, playerSpeed);
+	}
+}
 
 int main(){
 	sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "SFML works!");
 
-	texture.loadFromFile("dog.png");
-	sprite.setTextureRect(sf::IntRect(10, 10, 32, 32));
+	CreateTexture();
+	SetSprite(player);
+
 
 	while (window.isOpen())
 	{
+		//sf::Time elapsed = clock.restart();
+		//updateGame(elapsed);
 		sf::Event event;
 		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
+		{		
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
 				window.close();
+			}
+			PlayerMovement();
 		}
 
 		window.clear();
-		window.draw(sprite);
+		window.draw(player);
 		window.display();
 	}
 
